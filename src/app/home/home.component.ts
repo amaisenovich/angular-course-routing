7484db008dc233d4772add74a48f33ab3fafd79b
Aliaksei Maisenovich <a.maisenovich@gmail.com>
2022-01-07 14:51:59 +0300

    section 11.149

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/services/auth.service';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,13 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit() {
   }
 
-  onOpenServersClick = (serverId: number) => {
-    this.router.navigate(['/servers', 1, 'edit'])
-  }
+  onLoginClick = this.authService.login
+
+  onLogoutClick = this.authService.logout
 }
